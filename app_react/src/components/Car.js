fc852906dedc5a3586fd89b5d03eb817992e8679
JojoTartiflette <jolan.maggiochi@epitech.eu>
2020-10-01 19:21:22 +0200

    Use setState, callback function and change the rendering

--- app_react/src/components/Car.js
+++ app_react/src/components/Car.js
@@ -2,13 +2,33 @@ import React from 'react';
 
 class Car extends React.Component
 {
+    constructor() {
+        super();
+        this.state = {
+            brand: 'Ford',
+            model: 'Mustang',
+            color: 'red',
+            year: 1964,
+        };
+    };
+
+    changeColor = () => {
+        this.setState({ color: 'blue' }, () => {
+            console.log(this.state.color);
+        });
+    }
+
     render() {
         return (
             <>
-                <h2>Je suis une voiture</h2>
+                <h1>My { this.state.brand }</h1>
+                <p>
+                    It is a { this.state.color } { this.state.model } from { this.state.year }
+                </p>
+                <button type="button" onClick={this.changeColor}>Change Color</button>
             </>
-        )
-    }
+        );
+    };
 }
 
 export default Car;
