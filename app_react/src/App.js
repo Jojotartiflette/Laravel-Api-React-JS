29940f2b9f61e0b6db869229c3fe15cf656799c1
JojoTartiflette <jolan.maggiochi@epitech.eu>
2020-10-01 18:22:03 +0200

    Create Components

--- app_react/src/App.js
+++ app_react/src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
+import Car from './components/Car';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Welcome</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <Car/>
+      <Welcome/>
+    </>
   );
 }
 
---
+++ app_react/src/components/Car.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Car extends React.Component
+{
+    render() {
+        return (
+            <>
+                <h2>Je suis une voiture</h2>
+            </>
+        )
+    }
+}
+
+export default Car;
