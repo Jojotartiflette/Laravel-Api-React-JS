28120d22bbb8f2f3df9192f22c2a676110139371
JojoTartiflette <jolan.maggiochi@epitech.eu>
2020-10-01 15:43:34 +0200

    Add Controllers

--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -14,10 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/users', function() {
-    return response()->json(['success' => 'Method GET']);
-});
-
-Route::post('/users', function(Request $request) {
-    return $request->all();
-});
+Route::get('/users', 'TestController@getMethod');
+Route::post('/users', 'TestController@postMethod');
