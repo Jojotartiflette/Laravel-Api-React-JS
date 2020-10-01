10eabcba66aac4c6f1b5229f6146864a27289a54
JojoTartiflette <jolan.maggiochi@epitech.eu>
2020-10-01 17:12:15 +0200

    Make Middleware for header

--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/users', 'TestController@getMethod');
 Route::post('/users', 'TestController@postMethod');
 
-Route::post('/photos', 'PhotoController@store');
+Route::post('/photos', 'PhotoController@store')->middleware('App\Http\Middleware\PhotoMiddleware');
