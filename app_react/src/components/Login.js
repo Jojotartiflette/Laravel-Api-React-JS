dde1545c99fac182e8d12e83c135fbdff0fad36c
JojoTartiflette <jolan.maggiochi@epitech.eu>
2020-10-02 15:34:23 +0200

    Use LocalStorage

--- app_react/src/components/Login.js
+++ app_react/src/components/Login.js
@@ -5,24 +5,38 @@ class Login extends React.Component
     constructor() {
         super();
         this.state = {
-
+            email: '',
+            password: '',
         };
     }
 
-    email = event => {
-        console.log('Email Changed');
+    onEmailChange = event => {
+        this.setState({ email : event.target.value }, () => {
+            console.log('Email Changed');
+        });
+    }
+
+    onPasswordChange = event => {
+        this.setState({ password : event.target.value }, () => {
+            console.log('Password Changed');
+        });
+    }
+
+    handleFormSubmit = event => {
+        event.preventDefault();
+        localStorage.setItem('token', 'abcd');
     }
 
     render() {
         return (
             <>
-                <form method="POST">
-                    <input type="text" name="email" placeholder="Votre email" onChange={this.email}/>
-                    <input type="password" name="password" placeholder="Votre password" />
+                <form method="POST" onSubmit={this.handleFormSubmit}>
+                    <input type="text" name="email" placeholder="Votre email" onChange={this.onEmailChange}/>
+                    <input type="password" name="password" placeholder="Votre password" onChange={this.onPasswordChange}/>
                     <button type="submit">Me connecter</button>
                 </form>
             </>
-        )
+        );
     }
 }
 
--- app_react/src/components/Photos.js
+++ app_react/src/components/Photos.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import { Redirect } from 'react-router-dom';
 
 class Photos extends React.Component
 {
@@ -7,27 +8,38 @@ class Photos extends React.Component
         super();
         this.state = {
             photos: [],
+            redirect: false,
         };
     }
 
     componentDidMount() {
-        axios.get('http://127.0.0.1:8000/api/photos')
+        if (localStorage.getItem('token')) {
+            axios.get('http://127.0.0.1:8000/api/photos')
             .then(res => {
                 this.setState({ photos : res.data }, () => {
                     console.log(this.state);
-                })
+                });
             })
             .catch(error => console.log(error.response));
+        } else {
+            this.setState({ redirect: true });
+        }
     }
 
     render() {
+        if (this.state.redirect === true) {
+            return (
+                <Redirect to="/login" />
+            );
+        }
+
         return (
             <>
                 <div>
                     { this.state.photos.map(photo => <h1 key={photo.title}>{photo.title}</h1>) }
                 </div>
             </>
-        )
+        );
     }
 }
 
