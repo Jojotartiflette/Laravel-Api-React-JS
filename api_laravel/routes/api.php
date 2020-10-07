44a331000898a57af8edfa4305d43164c2c6c51a
JojoTartiflette <jolan.maggiochi@epitech.eu>
2020-10-07 12:56:24 +0200

    Clean app before start the project

--- api_laravel/routes/api.php
+++ api_laravel/routes/api.php
@@ -13,6 +13,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::get('/photos', 'PhotoController@index');
-Route::post('/photos', 'PhotoController@store')->middleware('App\Http\Middleware\PhotoMiddleware');
--- app_react/src/App.js
+++ app_react/src/App.js
@@ -1,41 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Welcome from './components/Welcome';
-import Car from './components/Car';
-import Login from './components/Login';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import Photos from './components/Photos';
 
 function App() {
   return (
     <>
-      <Router>
-        <div>
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="/login">Login</Link>
-              </li>
-              <li>
-                <Link to="/car">Car</Link>
-              </li>
-              <li>
-                <Link to="/photos">Photos</Link>
-              </li>
-            </ul>
-          </nav>
-          <Switch>
-            <Route exact path="/" component={Welcome}/>
-            <Route path="/login" component={Login}/>
-            <Route path="/car" component={Car}/>
-            <Route path="/photos" component={Photos}/>
-          </Switch>
-        </div>
-      </Router>
+      <Welcome />
     </>
   );
 }
